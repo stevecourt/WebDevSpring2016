271c8327852534ab6a13ad9c1e9bf5b4f04eeb72
STEVE COURT <court.s@husky.neu.edu>
2016-01-20 13:48:37 -0800

    new hello message

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || '3000';
 
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+//app.get('/hello', function(req, res){
+//    res.send('hello world');
+//});
 
 app.listen(port, ipaddress);
