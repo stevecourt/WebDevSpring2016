66c6f954911285bc3a0d3de065f237693edf8789
STEVE COURT <court.s@husky.neu.edu>
2016-01-22 13:01:29 -0800

    Added hello.js and changed server.js so hello message is at the root

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || '3000';
 
-app.get('/hello', function(req, res){
+app.get('/', function(req, res){
     res.send('hello world!');
 });
 
