10446c5e518510512262d42e70f0afdf876847e7
STEVE COURT <court.s@husky.neu.edu>
2016-02-03 17:24:54 -0800

    Updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var app = express();
-
-//app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || '3000';
-
-app.get('/', function(req, res){
-    res.send('hello world!');
-});
-
 app.listen(port, ipaddress);
