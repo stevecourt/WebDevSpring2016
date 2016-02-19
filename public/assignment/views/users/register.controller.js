6e0dedf80d88ae1250679f23ad4330f570611ee1
STEVE COURT <court.s@husky.neu.edu>
2016-02-19 14:59:03 -0800

    Temporary mod to render sidebar and navbar.

--- public/assignment/views/users/register.controller.js
+++ public/assignment/views/users/register.controller.js
@@ -1,1 +1,12 @@
 "use strict";
+
+(function(){
+    angular
+        .module("FormBuilderApp", ["ngRoute"])
+        .controller("RegisterController", RegisterController);
+    // $scope, $location - USE THESE PARAMS???
+    function RegisterController() {
+        //BODY HERE
+        //$scope.$location = $location;
+    }
+})();
