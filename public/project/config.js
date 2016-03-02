d3da8c85b3988da742bd44fa426f218f1ed6ce77
STEVE COURT <court.s@husky.neu.edu>
2016-03-01 16:50:54 -0800

    Added first placeholder pages for project POC.

---
+++ public/project/config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+(function () {
+    angular
+        .module("FormBuilderApp", ["ngRoute"])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when("/home", {
+                    templateUrl: "views/home/home.view.html",
+                    controller: "HomeController"
+                })
+                .when("/forms", {
+                    templateUrl: "views/forms/forms.view.html",
+                    controller: "FormController"
+                })
+                .when("/register", {
+                    templateUrl: "views/users/register.view.html",
+                    controller: "RegisterController"
+                })
+                .when("/login", {
+                    templateUrl: "views/users/login.view.html",
+                    controller: "LoginController"
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html",
+                    controller: "ProfileController"
+                })
+                .when("/admin", {
+                    templateUrl: "views/admin/admin.view.html",
+                    controller: "AdminController"
+                })
+                .otherwise({
+                    redirectTo: "/home"
+                });
+        });
+})();
