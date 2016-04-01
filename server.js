81a1d25af87ca3820395dd27072ea9f0ff79cf09
STEVE COURT <court.s@husky.neu.edu>
2016-03-31 23:09:53 -0700

    Origin test17.

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 }
 */
 //var db = mongoose.connect(connectionString);
-mongoose.connect(connectionString);
-
+//mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || '3000';
