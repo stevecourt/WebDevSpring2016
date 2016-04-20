41d241bc288b4f8b9ef3e343557076f93a43b086
STEVE COURT <court.s@husky.neu.edu>
2016-04-20 14:03:57 -0700

    Commit #3 to debug OpenShift issue.

--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -191,7 +191,7 @@ module.exports = function (app, userModel) {
 
         var newUser = req.body;
         newUser.emails = newUser.emails.split(",");
-        newUser.roles = ["admin"];
+        newUser.roles = ["student"];
         newUser.password = bcrypt.hashSync(newUser.password);
 
         userModel
